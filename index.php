3017a2a3578ba4eb26d8d076d8b73b2e43818cc2
AhmadFaozan <afaozan47@gmail.com>
2022-12-19 10:00:42 +0700

    project web stop violence

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Kekerasan Seksual</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css" rel="stylesheet" >
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" rel="stylesheet">
+  <link href="https://unpkg.com/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free-5.15.3-web/css/all.css" rel="stylesheet"/>
+
+  <!--  Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="index.php">STOP-VIOLENCE</a> <a href="index.php"><img src="assets/img/favicon.png" alt="" class="img-fluid"></a></h1>
+
+        
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active" href="index.php">Home</a></li>
+          <li><a href="jenis.html">Jenis Kekerasan Sex</a></li>
+          <li><a href="datadanstatistik.html">Data dan Statistik</a></li>
+          <li><a href="cegah.html">Pencegahan Kekerasan Seks</a></li>
+          <li><a href="contact.html">Kontak</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background: url(assets/img/slide/slide-1.png);
+          background-position: center;
+          background-size: cover;
+          position: relative;
+          background-attachment: fixed;">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Selamat Datang di <span>STOP-VIOLENCE</span></h2>
+                <p class="animate__animated animate__fadeInUp">Website ini dibuat untuk mengedukasi para pengunjung tentang Kekerasan Seksual, Jenis Kekerasan Seksual dan Cara Pencegahannya, 
+                  selain itu website ini juga menyediakan data dan statistik Kekerasan Seksual yang ada di indonesia maupun diseluruh dunia, 
+                cobalah fitur-fitur yang ditawarkan website ini secara gratis</p>
+                <a href="" class="btn-get-started animate__animated animate__fadeInUp">Pencegahan Kekerasan Seksual</a>
+              </div>
+            </div>
+          </div>
+       
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <h3>Apa itu STOP-VIOLENCE SEX ?</h3>
+            <p class="fst-italic">
+              STOP-VIOLENCE adalah website yang dikembangkan oleh Mahasiswa Politeknik Harapan Bersama Tegal untuk mengedukasi masyarakat 
+              seputar Kekerasan tentang Kekerasan Seksual di Lingkungan Kampus.
+            </p>
+
+            <p>
+              Website ini memiliki beberapa fitur yang bisa kamu manfaatkan untuk menambahkan pemahaman kamu tentang Kekerasan Seksual
+              , berikut beberapa fitur yang bisa kamu manfaatkan diantaranya.
+            </p>
+
+            <ul>
+              <li><i class="bi bi-check-circle"></i> Edukasi Pencegahan Seksual.</li>
+              <li><i class="bi bi-check-circle"></i> Data dan Statistik Kekerasan Seksual.</li>
+              <li><i class="bi bi-check-circle"></i> Jenis-Jenis Kekerasan Seksual.</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Featured Section ======= -->
+    <section id="featured" class="featured">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="icon-box">
+              <i class="bi bi-shield-fill-plus"></i>
+              <h3><a href="cegah.html">Pencegahan Kekerasan Seksual</a></h3>
+              <p>Kenali lebih lanjut tentang Kekerasan Seksual dan cara pencegahannya</p>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="icon-box">
+              <i class="bi bi-graph-up"></i>
+              <h3><a href="datadanstatistik.html">Data dan Statistik</a></h3>
+              <p>Data dan statistik terkini tentang Kekerasan Seksual yang ada di Indonesia maupun dunia</p>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="icon-box">
+              <i class="bi bi-card-checklist"></i>
+              <h3><a href="jenis.html">Jenis</a></h3>
+              <p>Mengetahui jenis-jenis kekerasan seksual saat ini.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Featured Section -->
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top" >
+      <div class="container" >
+        <div class="row justify-content-center">
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="jenis.html">Jenis Kekerasan Sex</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="datadanstatistik.html">Data dan Statistik</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="cegah.html">Pencegahan Kekerasan Seksual</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Kontak</a></li>
+            </ul>
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Hubungi Kami</h4>
+            <p>
+            Jl. Mataram No.9, Kel. pesurungan lor,<br>
+            Kel. Pesurungan Lor, Pesurungan Lor, <br>
+            Kec. Margadana, Kota Tegal,<br>
+            Jawa Tengah 52147<br><br>
+              
+              <strong>Phone:</strong> (0283) 352000<br>
+              <strong>Email:</strong> sekretariat@poltektegal.ac.id<br>
+            </p>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>Tentang STOP-VIOLENCE</h3>
+            <p> STOP-VIOLENCE adalah website yang dikembangkan oleh Mahasiswa Politeknik Harapan Bersama Tegal untuk mengedukasi masyarakat 
+              seputar Kekerasan tentang Kekerasan Seksual di Lingkungan Kampus.</p>
+            <div class="social-links mt-3">
+              <a href="https://www.facebook.com/poltekharber.fanspage/" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.instagram.com/poltek_harber/" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="https://www.youtube.com/c/poltekharber" class="youtube"><i class="bx bxl-youtube"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>EDU-VIOLENCE</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="#">Kelompok 2-Leadership</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
+  <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!--  Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
